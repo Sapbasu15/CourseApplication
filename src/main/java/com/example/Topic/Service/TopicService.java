60327362be15e09f21ad77f6bd37c40869e7a645
saptarshi <sbasu2324@gmail.com>
2020-10-11 08:52:24 +0530

    added services for adding, updating and deleting topic

--- src/main/java/com/example/Topic/Service/TopicService.java
+++ src/main/java/com/example/Topic/Service/TopicService.java
@@ -1,4 +1,44 @@
 package com.example.Topic.Service;
 
+import com.example.Topic.Model.Topic;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@Service
 public class TopicService {
+
+    private static List<Topic> topics = new ArrayList<>(Arrays.asList(
+            new Topic("java", "Core Java", "Core Java Description"),
+                new Topic("spring", "Spring Boot", "Spring Boot Description" ),
+                new Topic("javascript", "ecma6", "ES6 description")
+        ));
+
+    public List<Topic> getAllTopics(){
+        return topics;
+    }
+
+    public Topic getTopicByID(String id){
+        return topics.stream().filter(t -> t.getId().equalsIgnoreCase(id)).findFirst().orElse(null);
+    }
+
+    public void addTopic(Topic topic){
+        topics.add(topic);
+    }
+
+    public void updateTopic(String id, Topic topic){
+        for(int i=0; i<topics.size(); i++){
+            if(topics.get(i).getId().equals(id)){
+                topics.set(i, topic);
+                break;
+            }
+        }
+    }
+
+    public void deleteTopic(String id){
+        topics.removeIf(topic -> topic.getId().equals(id));
+    }
+
 }
