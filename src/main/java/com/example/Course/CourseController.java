918223df86f3eb35bfd5169ac7c5bfca66196077
saptarshi <sbasu2324@gmail.com>
2020-11-15 20:12:28 +0530

    Added media types

--- src/main/java/com/example/Course/CourseController.java
+++ src/main/java/com/example/Course/CourseController.java
@@ -3,6 +3,7 @@ package com.example.Course;
 import com.example.Topic.Model.Topic;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -15,12 +16,12 @@ public class CourseController {
     @Autowired
     private CourseService courseService;
 
-    @RequestMapping("/courses")
+    @GetMapping(value = "/courses", produces = {MediaType.APPLICATION_JSON_VALUE })
     public List<Course> getCourse(@PathVariable String topicId){
         return courseService.getCoursesByTopicId(topicId);
     }
 
-    @RequestMapping("/courses/{courseId}")
+    @GetMapping(value = "/courses/{courseId}", produces = {MediaType.APPLICATION_JSON_VALUE})
     public Course getCourseById(@PathVariable String topicId, @PathVariable String courseId){
         Course course = courseService.getCourseById(courseId);
         if(course == null)
@@ -28,7 +29,7 @@ public class CourseController {
         return course;
     }
 
-    @PostMapping("/courses")
+    @PostMapping(value = "/courses", consumes = {MediaType.APPLICATION_JSON_VALUE})
     public void addCourse(@PathVariable String topicId, @RequestBody List<Course> courses){
         courses.forEach(course -> {
             course.setTopic(new Topic(topicId,"",""));
@@ -36,9 +37,9 @@ public class CourseController {
         });
     }
 
-    @PutMapping("/courses/{courseId}")
+    @PutMapping(value = "/courses/{courseId}", consumes = {MediaType.APPLICATION_JSON_VALUE})
     public void updateCourse(@PathVariable String topicId, @PathVariable String courseId,@RequestBody Course course){
-        course.setTopic(new Topic(topicId,"ww","qq"));
+        course.setTopic(new Topic(topicId,"topicName","topicDescription"));
         courseService.updateCourse(courseId, course);
     }
 
--- src/main/java/com/example/Topic/Controller/TopicController.java
+++ src/main/java/com/example/Topic/Controller/TopicController.java
@@ -3,6 +3,7 @@ package com.example.Topic.Controller;
 import com.example.Topic.Model.Topic;
 import com.example.Topic.Service.TopicService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
@@ -14,22 +15,22 @@ public class TopicController {
     @Autowired
     private TopicService topicService;
 
-    @RequestMapping("/topics")
+    @GetMapping(value = "/topics", produces = {MediaType.APPLICATION_JSON_VALUE})
     public List<Topic> getTopics(){
         return topicService.getAllTopics();
     }
 
-    @RequestMapping("/topics/{id}")
+    @GetMapping(value = "/topics/{id}", produces = {MediaType.APPLICATION_JSON_VALUE})
     public Topic getTopicByID(@PathVariable String id){
         return topicService.getTopicByID(id);
     }
 
-    @RequestMapping(method = RequestMethod.POST, value = "/topics")
+    @PostMapping(value = "/topics", consumes = {MediaType.APPLICATION_JSON_VALUE})
     public void addTopic(@RequestBody List<Topic> topics){
         topics.forEach(topic -> topicService.addTopic(topic));
     }
 
-    @RequestMapping(method = RequestMethod.PUT, value="/topics/{id}")
+    @RequestMapping(method = RequestMethod.PUT, value="/topics/{id}", consumes = {MediaType.APPLICATION_JSON_VALUE})
     public void updateTopic(@PathVariable String id, @RequestBody Topic topic){
         topicService.updateTopic(id, topic);
     }
